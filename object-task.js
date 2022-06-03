3ef51b0f27e733f6d7d76856527528f4530d96c3
Alumiantro <zaikamarta@mail.ru>
2022-06-03 17:22:37 +0300

    added the solution to the problem by objects

--- object-task.js
+++ object-task.js
@@ -1,6 +1,7 @@
 // 1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
 // 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
 // Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+// Создайте метод showAgeAndLangs внутри объекта personalPlanPeter внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде. Заметьте, языки подставляются всегда в верхнем регистре.
 
 const personalPlanPeter = {
     name: "Peter",
@@ -12,7 +13,18 @@ const personalPlanPeter = {
             php: '10%'
         },
         exp: '1 month'
-    }
+    },
+    showAgeAndLangs: function(obj) {
+        const {languages} = obj.skills;
+        const {age} = obj;
+        let str = `Мне ${age} и я владею языками:`;
+  
+        languages.forEach(function(item) {
+            str += ` ${item.toUpperCase()}`
+        })
+        
+        console.log(str);
+      }
 };
 
 function showExperience(plan) {
