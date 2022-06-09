57dbdd8c3699f250f2a2034c69c1cb64348889cb
Alumiantro <zaikamarta@mail.ru>
2022-06-09 12:53:43 +0300

    added solution to tasks from interviews

---
+++ task-from-inreviews.js
@@ -0,0 +1,51 @@
+//* 1 Какое будет выведено значение: let x = 5; alert( x++ ); ?
+let x = 5;
+console.log (x++) //5
+
+
+//* Чему равно такое выражение: [] + false - null + true ?
+console.log([] + false - null + true) //NaN
+
+
+//* Что выведет этот код: let y = 1; let x = y = 2; alert(x);
+let y = 1;
+let c = y = 2;
+console.log(c); //2
+
+
+//* Чему равна сумма [] + 1 + 2?
+console.log([] + 1 + 2) //12
+
+
+//* Что выведет этот код: alert("1"[0])?
+console.log("1"[0]) //1 Обращаемся к индексу элемента
+
+
+//* Чему равно
+console.log(2 && 1 && null && 0 && undefined) //null
+
+
+//* Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+// Да. В первом случае вернется булиновое значение, во втором - b
+
+
+//* Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
+console.log( null || 2 && 3 || 4 ) //3
+
+
+//* a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+a = [1, 2, 3]
+b = [1, 2, 3]
+console.log(a == b) //false
+
+
+//* Что выведет этот код: alert(+"Infinity"); ?
+console.log(+"Infinity") //Infinity
+
+
+//* Верно ли сравнение: 'Ёжик' > 'Яблоко'?
+console.log('Ёжик' > 'Яблоко') //false
+
+
+//* Чему равно 0 || "" || 2 || undefined || true || falsе ?
+console.log(0 || "" || 2 || undefined || true || falsе) //2
