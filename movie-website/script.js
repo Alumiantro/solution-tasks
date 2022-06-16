961227be8388d7da9d04584d11dd66f0a4b9c086
Alumiantro <zaikamarta@mail.ru>
2022-06-16 17:25:48 +0300

    edit solution

--- movie-website/script.js
+++ movie-website/script.js
@@ -1,43 +1,11 @@
-/* Задания на урок:
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const { movies } = movieDB;
-const promo = document.querySelectorAll(".promo__adv img");
-const promoGenre = document.querySelector('.promo__genre');
-const promoBg = document.querySelector('.promo__bg');
-const promoList = document.querySelector('.promo__interactive-list');
-
-promo.forEach(item => {
-    item.remove()
-})
-promoGenre.textContent = 'драма'
-promoBg.style.background = 'url(img/bg.jpg) top center / cover no-repeat'
-movies.sort()
-promoList.innerHTML = ''
-movies.forEach((item, index) => {
-    promoList.innerHTML +=
-        `<li class="promo__interactive-item">${index + 1} ${item}
-        <div class="delete"></div>
-    </li>`
-})
-
+//* Задания на урок:
+// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+// 2) Изменить жанр фильма, поменять "комедия" на "драма"
+// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+// Реализовать только при помощи JS
+// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+// Отсортировать их по алфавиту 
+// 5) Добавить нумерацию выведенных фильмов
 
 //* Задания:
 // 1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
@@ -51,41 +19,91 @@ movies.forEach((item, index) => {
 // "Добавляем любимый фильм"
 // 5) Фильмы должны быть отсортированы по алфавиту
 
-const formButton = document.getElementById('add_val')
-const favouriteFilm = document.getElementById('fav_film')
-const basketForDelete = document.querySelectorAll('.delete')
+'use strict';
 
-formButton.addEventListener('click', addedValue)
-favouriteFilm.addEventListener('click', addedFav)
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
+    const { movies } = movieDB;
+    const promo = document.querySelectorAll(".promo__adv img");
+    const promoGenre = document.querySelector('.promo__genre');
+    const promoBg = document.querySelector('.promo__bg');
+    const promoList = document.querySelector('.promo__interactive-list');
 
-function addedValue(event) {
-    event.preventDefault();
-    const input = document.getElementById('get_val').value;
-
-    if (input.length > 21) {
+    promo.forEach(item => {
+        item.remove()
+    })
+    promoGenre.textContent = 'драма'
+    promoBg.style.background = 'url(img/bg.jpg) top center / cover no-repeat'
+    movies.sort()
+    promoList.innerHTML = ''
+    movies.forEach((item, index) => {
         promoList.innerHTML +=
-        `<li class="promo__interactive-item">${+promoList.lastChild.textContent[0] + 1} ${input.slice(0, 21)}...
-        <div class="delete"></div>
+            `<li class="promo__interactive-item">${index + 1} ${item}
+            <div class="delete"></div>
         </li>`
-    } else {
-        promoList.innerHTML +=
-        `<li class="promo__interactive-item">${+promoList.lastChild.textContent[0] + 1} ${input}
-        <div class="delete"></div>
-        </li>`
+    })
+
+    const formButton = document.getElementById('add_val')
+    const favouriteFilm = document.getElementById('fav_film')
+    const basketForDelete = document.querySelectorAll('.delete')
+
+    formButton.addEventListener('click', addedValue)
+    // favouriteFilm.addEventListener('click', addedFav)
+
+
+    function addedValue(event) {
+        event.preventDefault();
+        const input = document.getElementById('get_val').value;
+
+        if (input.length > 21) {
+            promoList.innerHTML +=
+                `<li class="promo__interactive-item">${+promoList.lastChild.textContent[0] + 1} ${input.slice(0, 21)}...
+            <div class="delete"></div>
+            </li>`
+        } else {
+            promoList.innerHTML +=
+                `<li class="promo__interactive-item">${+promoList.lastChild.textContent[0] + 1} ${input}
+            <div class="delete"></div>
+            </li>`
+        }
+
+        if (favouriteFilm.checked) {
+            console.log('Добавляем любимый фильм')
+        }
     }
-    
-    console.log(promoList)
-}
 
-function addedFav() {
-    console.log('Добавляем любимый фильм')
-}
+    basketForDelete.forEach((item, index) => {
+        item.addEventListener('click', function deleteFilm() {
+            promoList.childNodes[index].remove()
+        })
+    })
 
-basketForDelete.forEach((item, index) => {
-    item.addEventListener('click', deleteFilm)
+    // console.log(movies.sort())
+    // console.log(promoList)
+    // promoList.forEach((item) => {
+    //     item.sort()
+    // })
 })
 
-function deleteFilm() {
-    // .remove()
-}
+
+//touchstart
+//touchmove 
+
+document.addEventListener('DOMContentLoader', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault()
+
+        console.log("start")
+    })
+})
